b5798a1faf5fc7039532efedcf6d6b1025e78b1c
aaronaes <aaronaes@mac.com>
2019-12-16 16:10:58 +0100

    Updated GL case and images

--- src/projects/godt-levert.vue
+++ src/projects/godt-levert.vue
@@ -4,15 +4,25 @@
       <figure class="columns title">
         <h2>Godt Levert</h2>
         <p>A food-to-your-door subscription service.</p>
-        <a href="#gl" @click="showMore = !showMore" class="toggle" :class="{ opened: showMore}">
+        <a
+          href="#gl"
+          @click="showMore = !showMore"
+          class="toggle"
+          :class="{ opened: showMore }"
+        >
           <p>
             <span></span>
           </p>
         </a>
         <div class="toggleInfo" v-show="showMore">
           <p class="copy">
-            Godt Levert didn't have as much of a presence in the market as they wanted. The service was new and was gaining traction, and with that Godt Levert wanted to become a household name.
-            The solution to this was taking a more holistic approach to their product, through creating a new way to browse meals and weekly deals. As a part of the project, the whole app was revamped with new UI and a new website.
+            Godt Levert didn't have as much of a presence in the market as they
+            wanted. The service was new and was gaining traction, and with that
+            Godt Levert wanted to become a household name. The solution to this
+            was taking a more holistic approach to their product, through
+            creating a new way to browse meals and weekly deals. As a part of
+            the project, the whole app was revamped with new UI and a new
+            website.
           </p>
           <p class="link">
             <a href="//www.godtlevert.no" target="_blank">Visit site &nearr;</a>
@@ -21,31 +31,31 @@
       </figure>
 
       <figure class="columns">
-        <img v-lazy="`/images/work/gl/gl-01.jpg`" />
+        <img v-lazy="`/images/work/gl/gl-01.png`" />
       </figure>
       <figure class="columns">
-        <img v-lazy="`/images/work/gl/gl-02.jpg`" />
+        <img v-lazy="`/images/work/gl/gl-02.png`" />
       </figure>
       <figure class="columns">
-        <img v-lazy="`/images/work/gl/gl-03.jpg`" />
+        <img v-lazy="`/images/work/gl/gl-03.png`" />
       </figure>
       <figure class="columns">
-        <img v-lazy="`/images/work/gl/gl-04.jpg`" />
+        <img v-lazy="`/images/work/gl/gl-04.png`" />
       </figure>
       <figure class="columns">
-        <img v-lazy="`/images/work/gl/gl-05.jpg`" />
+        <img v-lazy="`/images/work/gl/gl-05.png`" />
       </figure>
       <figure class="columns">
-        <img v-lazy="`/images/work/gl/gl-06.jpg`" />
+        <img v-lazy="`/images/work/gl/gl-06.png`" />
       </figure>
       <figure class="columns">
-        <img v-lazy="`/images/work/gl/gl-07.jpg`" />
+        <img v-lazy="`/images/work/gl/gl-07.png`" />
       </figure>
       <figure class="columns">
-        <img v-lazy="`/images/work/gl/gl-08.jpg`" />
+        <img v-lazy="`/images/work/gl/gl-08.png`" />
       </figure>
       <figure class="columns">
-        <img v-lazy="`/images/work/gl/gl-09.jpg`" />
+        <img v-lazy="`/images/work/gl/gl-09.png`" />
       </figure>
     </section>
   </section>
