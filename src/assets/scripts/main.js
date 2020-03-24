220874c0cbb2eb3db78744fb7cc3e1fdb09ee5cc
leifriksheim <riksheim.leif@gmail.com>
2020-03-24 08:16:37 -0600

    remove lots of code

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,15 +1,6 @@
-import "@/assets/scripts/general.js";
 import "@/assets/scripts/noHover.js";
 import "@/assets/scripts/bg.js";
 import "@/assets/scripts/scrollTo.js";
-import "@/assets/scripts/carousel.js";
 import "@/assets/scripts/header.js";
 import "@/assets/scripts/parallax.js";
 import "@/assets/scripts/toggleMenu.js";
-import "@/assets/scripts/showMore.js";
-// import "@/assets/scripts/localWeather.js";
-// import "@/assets/scripts/hero.js";
-// import "@/assets/scripts/cursor.js";
-// import "@/assets/scripts/fontUp.js";
-// import "@/assets/scripts/colorBg.js";
-// import "@/assets/scripts/mousey.js";
--- src/main.js
+++ src/main.js
@@ -3,15 +3,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import "./../node_modules/foundation-sites/dist/css/foundation.min.css";
-import "./../node_modules/foundation-sites/dist/js/foundation.min.js";
+import "foundation-sites/dist/css/foundation.min.css";
+import "foundation-sites/dist/js/foundation.min.js";
 import "./assets/scripts/main.js";
 import "./assets/styles/main.scss";
-import vueScrollto from "vue-scrollto";
 import VueLazyload from "vue-lazyload";
 
 Vue.config.productionTip = false;
-Vue.use(vueScrollto);
 Vue.use(VueLazyload);
 
 new Vue({
