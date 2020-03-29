fc3b46fff7981b852ea1899ac8e46c4098cc0e4c
aaronaes <aaronaes@mac.com>
2020-03-29 19:05:06 +0200

    Fixed crashed shit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,17 +4,20 @@
       <figure class="grid-x grid-margin-x">
         <span class="shrink">
           <a href="//www.instagram.com/duties.xyz" target="_blank">
-            <p>new@duties.xyz</p>
+            <p class="hide-for-small-only">new@duties.xyz</p>
+            <p class="show-for-small-only">new@duties.xyz</p>
           </a>
           <a href="//www.instagram.com/duties.xyz" target="_blank">
-            <p>Facebook</p>
+            <p class="hide-for-small-only">Facebook</p>
+            <p class="show-for-small-only">FB</p>
           </a>
           <a href="//www.instagram.com/duties.xyz" target="_blank">
-            <p>Instagram</p>
+            <p class="hide-for-small-only">Instagram</p>
+            <p class="show-for-small-only">Insta</p>
           </a>
         </span>
         <span
-          ><a href="#top" class="shrink ">
+          ><a href="#top" class="shrink top">
             <p class="arrow">&uarr;</p>
           </a></span
         >
