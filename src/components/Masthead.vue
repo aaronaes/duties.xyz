46fb661ebdd433b13b167190b86f076cdb297532
aaronaes <aaronaes@mac.com>
2020-03-30 15:51:26 +0200

    Updated minnor bug fixes

--- src/components/Masthead.vue
+++ src/components/Masthead.vue
@@ -24,9 +24,9 @@
                 "
               >
                 <svg
-                  width="60"
-                  height="43"
-                  viewBox="0 0 60 43"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 60 40"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
