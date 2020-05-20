bd1b01b007db2aa703f45c0a17ad7deffea3c9fc
aaronaes <aaronaes@mac.com>
2020-05-20 11:17:34 +0200

    Updated link hover on touch devices

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -5,72 +5,60 @@
         <h3 class="section-title">Links &searr;</h3>
         <details class="cell">
           <summary class="grid-x align-middle">
-            <h2>
-              <span class="dot">●</span>
-              <span class="title">Medium</span>
-            </h2>
-
-            <h2 class="togl arrow">
-              <a href="https://medium.com/@duties.xyz" target="_blank">Link</a>
-            </h2>
+            <a href="https://medium.com/@duties.xyz" target="_blank">
+              <h2>
+                <span class="dot">●</span>
+                <span class="title">Medium</span>
+              </h2>
+              <h2 class="togl arrow">Link</h2>
+            </a>
           </summary>
         </details>
         <details class="cell">
           <summary class="grid-x align-middle">
-            <h2>
-              <span class="dot">●</span>
-              <span class="title">Spotify</span>
-            </h2>
-
-            <h2 class="togl arrow">
-              <a
-                href="https://open.spotify.com/playlist/39NC0tBJ0FrrqBD8Tj156m?si=HS5YF10BQfmFFN6VHJtYOQ"
-                target="_blank"
-                >Link</a
-              >
-            </h2>
+            <a
+              href="https://open.spotify.com/playlist/39NC0tBJ0FrrqBD8Tj156m?si=HS5YF10BQfmFFN6VHJtYOQ"
+              target="_blank"
+            >
+              <h2>
+                <span class="dot">●</span>
+                <span class="title">Spotify</span>
+              </h2>
+              <h2 class="togl arrow">Link</h2>
+            </a>
           </summary>
         </details>
         <details class="cell">
           <summary class="grid-x align-middle">
-            <h2>
-              <span class="dot">●</span>
-              <span class="title">LinkedIn</span>
-            </h2>
-
-            <h2 class="togl arrow">
-              <a href="https://www.linkedin.com/company/duties/" target="_blank"
-                >Link</a
-              >
-            </h2>
+            <a href="https://www.linkedin.com/company/duties/" target="_blank">
+              <h2>
+                <span class="dot">●</span>
+                <span class="title">LinkedIn</span>
+              </h2>
+              <h2 class="togl arrow">Link</h2>
+            </a>
           </summary>
         </details>
         <details class="cell">
           <summary class="grid-x align-middle">
-            <h2>
-              <span class="dot">●</span>
-              <span class="title">Dribbble</span>
-            </h2>
-
-            <h2 class="togl arrow">
-              <a href="https://dribbble.com/duties/collections" target="_blank"
-                >Link</a
-              >
-            </h2>
+            <a href="https://dribbble.com/duties/collections" target="_blank">
+              <h2>
+                <span class="dot">●</span>
+                <span class="title">Dribbble</span>
+              </h2>
+              <h2 class="togl arrow">Link</h2>
+            </a>
           </summary>
         </details>
         <details class="cell">
           <summary class="grid-x align-middle">
-            <h2>
-              <span class="dot">●</span>
-              <span class="title">Facebook</span>
-            </h2>
-
-            <h2 class="togl arrow">
-              <a href="https://www.facebook.com/duties.xyz" target="_blank"
-                >Link</a
-              >
-            </h2>
+            <a href="https://facebook.com/dutiesxyz" target="_blank">
+              <h2>
+                <span class="dot">●</span>
+                <span class="title">Facebook</span>
+              </h2>
+              <h2 class="togl arrow">Link</h2>
+            </a>
           </summary>
         </details>
       </figure>
