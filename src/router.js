7dcdf85730f20086ea9d142eb75ce0a624464fbb
leifriksheim <riksheim.leif@gmail.com>
2020-08-14 12:51:47 +0200

    remove hardcoded routes

--- src/router.js
+++ src/router.js
@@ -29,41 +29,6 @@ export default new Router({
       name: "About",
       component: () =>
         import(/* webpackChunkName: "About" */ "./views/About.vue")
-    },
-    {
-      path: "/gl",
-      name: "Godt Levert",
-      component: () => import(/* webpackChunkName: "GL" */ "./projects/gl.vue")
-    },
-    {
-      path: "/eika",
-      name: "Eika Gruppen",
-      component: () =>
-        import(/* webpackChunkName: "Eika" */ "./projects/eika.vue")
-    },
-    {
-      path: "/gro",
-      name: "Gro Grønt",
-      component: () =>
-        import(/* webpackChunkName: "Gro Gront" */ "./projects/gro.vue")
-    },
-    {
-      path: "/ogle",
-      name: "Ogle Image",
-      component: () =>
-        import(/* webpackChunkName: "Eika" */ "./projects/ogle.vue")
-    },
-    {
-      path: "/humid",
-      name: "Humid Mag",
-      component: () =>
-        import(/* webpackChunkName: "Eika" */ "./projects/humid.vue")
-    },
-    {
-      path: "/marks",
-      name: "Logos and wordmarks",
-      component: () =>
-        import(/* webpackChunkName: "Eika" */ "./projects/humid.vue")
     }
   ]
 });
